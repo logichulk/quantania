6bc30d8eb8e72340c139983e3b2226301d81b23a
Tushar Sikand <tusharsikand@gmail.com>
2020-07-26 11:26:11 +0530

    Autocommit at 2020-07-26 11:26:11.408978

--- autocommit.py
+++ autocommit.py
@@ -1,16 +1,19 @@
 import subprocess as cmd
+#import paramiko
 
 from datetime import datetime
 
 # Auto-commit script for content.
+
+#ssh_client = paramiko.SSHClient()
+#ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#ssh_client.connect(hostname='https://github.com/logichulk/books.git',username='logichulk',password='F22rptr')
 
 statements = list()
 
 statements.append("git add .")
 statements.append("git commit -m 'Autocommit at " + str(datetime.now()) + "'")
 statements.append("git push -u origin master -f")
-statements.append("logichulk")
-statements.append("F22rptr")
 
 for statement in statements:
 	cmd.call(statement, shell=True)
