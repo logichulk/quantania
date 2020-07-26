442c36922c58d03a554b17e9207a2f3ed7cd572b
Tushar Sikand <tusharsikand@gmail.com>
2020-07-26 22:14:59 +0530

    Sync

---
+++ utils/github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote show origin
+echo 'You can change this URL with for credential-less push with:'
+echo 'git remote set-url origin git+ssh://git@github.com/username/reponame.git'
