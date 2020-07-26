b2696cb7170af0edef42b0e42e67153f1f8a5490
Tushar Sikand <tusharsikand@gmail.com>
2020-07-26 11:38:43 +0530

    Sync

--- autocommit.py
+++ autocommit.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 #ssh_client = paramiko.SSHClient()
 #ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#ssh_client.connect(hostname='https://github.com/logichulk/books.git',username='logichulk',password='F22rptr')
+#ssh_client.connect(hostname='https://github.com/logichulk/books.git',username='',password='')
 
 statements = list()
 
